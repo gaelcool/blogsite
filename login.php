13e4f52d517b29f78b7f315a3128d2081bdbb177
MechygoesGLOBAL <22565325+gaelcool@users.noreply.github.com.>
2025-11-10 09:00:10 -0800

    Currently works, i think however that it doesnt feel inntuitive, also logout button should be visible through out the whole site when logged in

--- login.php
+++ login.php
@@ -4,12 +4,12 @@ session_start();
 
 $error = '';
 
-if ($_SERVER['REQUEST_METHOD'] === 'post')
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
     $pdo = getPDO();
     
     // Get credentials
-    $usuario = trim($_POST['user']);
+    $usuario = trim($_POST['usuario']);
     $clave = $_POST['clave'];
     
     try {
@@ -19,8 +19,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'post')
         if ($userData)
         {
             // Login successful
-            login($userData['user'], $userData['nombre'], $userData['genero_lit_fav']);
-
+            login($userData['usuario'], $userData['nombre'], $userData['genero_lit_fav']);
+            
             header('Location: LP.php');
             exit();
         }
